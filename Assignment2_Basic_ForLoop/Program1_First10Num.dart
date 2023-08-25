2b7362f00a2bad7bcc8b6d2b9b248878a47a8351
Ankita <ankitachougule7966@gmail.com>
2023-08-25 10:54:12 +0530

    commit Basic For in Dart

---
+++ Assignment2_Basic_ForLoop/Program1_First10Num.dart
@@ -0,0 +1,6 @@
+void main(){
+  print("First 10 Numbers");
+  for(int i=1;i<=10;i++){
+    print(i);
+  }
+}
---
+++ Assignment2_Basic_ForLoop/Program4_even.dart
@@ -0,0 +1,9 @@
+void main(){
+  print("Even Numbers from 1-100");
+
+  for(int i=1;i<=100;i++){
+    if(i%2==0){
+      print(i);
+    }
+  }
+}
